170542665c62adb32ab8d8a23b27be00f01d9cf3
Ruel Arguelles <arguellesruel03@gmail.com>
2026-07-29 13:30:45 +0800

    add logo to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,29 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Jesean Rentals",
   description: "Printer rentals, repairs, sales, and CCTV installations",
+  openGraph: {
+    title: "Jesean Printers",
+    description: "Printer rentals, repairs, sales, and CCTV installations",
+    url: "https://jesean-printer-rental.vercel.app/",
+    siteName: "Jesean Printers",
+    images: [
+      {
+        url: "/logo.png",
+        width: 1200,
+        height: 630,
+        alt: "Jesean Printers",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Jesean Printers",
+    description: "Printer rentals, repairs, sales, and CCTV installations",
+    images: ["/logo.png"],
+  },
 };
 
 export default function RootLayout({
